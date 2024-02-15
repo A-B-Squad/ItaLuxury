6d795e6477bb380cfe3e7577759a437e5177ff6f
Ahmed Haddada <haddadaahmed9@gmail.com>
2024-02-15 17:43:06 +0100

    graphql all query type for schema and some mutation query

--- my-app/prisma/db.ts
+++ my-app/prisma/db.ts
@@ -1,12 +1,19 @@
+// Import the PrismaClient class from the Prisma client library
 import { PrismaClient } from "@prisma/client";
 
+// Declare a variable named globalForPrisma and cast it as an object with a prisma property of type PrismaClient
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-console.log(globalForPrisma);
 
+// Create an instance of the PrismaClient class and assign it to the prisma variable.
+// If globalForPrisma.prisma already exists (i.e., if it was previously assigned), use that existing instance.
+// Otherwise, create a new instance of PrismaClient with logging enabled for queries.
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query"],
   });
 
+// If the current environment is not production, assign the prisma instance to globalForPrisma.prisma.
+// This ensures that the same prisma instance is used across different modules in the application,
+// preventing multiple connections to the database in non-production environments.
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
