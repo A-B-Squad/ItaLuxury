649f3a2101f891304430f3a694ca31c4c733e97a
Bilelbourgou <bilelbourgou@gmail.com>
2024-03-22 17:32:41 +0100

    product details functionallities completed

--- my-app/app/(mainApp)/products/[productId]/page.tsx
+++ my-app/app/(mainApp)/products/[productId]/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import React, { useState } from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useLazyQuery, gql } from "@apollo/client";
+import ReactImageMagnify from "react-image-magnify";
 
 const ProductDetails = ({ params }: { params: { productId: string } }) => {
-  console.log("====================================");
-  console.log(params.productId);
-  console.log("====================================");
   const [productDetails, setProductDetails] = useState<any>(null);
   const [bigImage, setBigImage] = useState<any>(null);
   const [smallImages, setSmallImages] = useState<any>(null);
-
+  const [colors, setColors] = useState<any>(null);
 
   const PRODUCT_BY_ID_QUERY = gql`
     query ProductById($productByIdId: ID!) {
@@ -25,22 +23,39 @@ const ProductDetails = ({ params }: { params: { productId: string } }) => {
         solde
         images
         createdAt
+        ProductColorImage {
+          Colors {
+            id
+            color
+            Hex
+          }
+        }
       }
     }
   `;
 
+  const GET_PRODUCT_IMAGES_QUERY = gql`
+    query Query($productId: String!, $colorId: String!) {
+      getProductImages(productId: $productId, colorId: $colorId)
+    }
+  `;
+  const [getProductImages] = useLazyQuery(GET_PRODUCT_IMAGES_QUERY);
+
   const productById = useQuery(PRODUCT_BY_ID_QUERY, {
     variables: { productByIdId: params.productId },
     onCompleted: (data) => {
-      console.log(data.productById);
       setProductDetails(data.productById);
       setBigImage(data.productById.images[0]);
       setSmallImages(data.productById.images);
+      setColors(
+        data.productById.ProductColorImage.map((image: any) => image.Colors)
+      );
     },
     onError: (error) => {
       console.log(error);
     },
   });
+
   return (
     <>
       {!!productDetails ? (
@@ -48,22 +63,32 @@ const ProductDetails = ({ params }: { params: { productId: string } }) => {
           <div className="p-6 lg:max-w-7xl max-w-2xl max-lg:mx-auto">
             <div className="grid items-start grid-cols-1 lg:grid-cols-5 gap-12">
               <div className="lg:col-span-3 w-full lg:sticky top-0 text-center">
-                <div className="bg-lightBeige px-4 py-10 rounded-xl">
-                  <img
-                    src={bigImage}
-                    alt="Product"
+                <div className="bg-lightBeige flex items-center justify-center px-4 py-10 rounded-xl">
+                  <ReactImageMagnify
                     className="w-4/5 rounded object-cover"
+                    {...{
+                      smallImage: {
+                        alt: "Wristwatch by Ted Baker London",
+                        isFluidWidth: true,
+                        src: bigImage,
+                      },
+                      largeImage: {
+                        src: bigImage,
+                        width: 1120,
+                        height: 1800,
+                      },
+                    }}
                   />
                 </div>
                 <div className="mt-6 flex flex-wrap justify-center gap-x-10 gap-y-6 mx-auto">
-                  {smallImages.map((image:string) => (
-                    <div className="bg-lightBeige rounded-xl p-4">
+                  {smallImages.map((image: string, index: number) => (
+                    <div key={index} className="bg-lightBeige rounded-xl p-4">
                       <img
                         src={image}
                         alt="Product2"
                         className="w-24 cursor-pointer"
-                        onClick={()=>{
-                          setBigImage(image)
+                        onClick={() => {
+                          setBigImage(image);
                         }}
                       />
                     </div>
@@ -146,22 +171,25 @@ const ProductDetails = ({ params }: { params: { productId: string } }) => {
                       Choisir une couleur
                     </h3>
                     <div className="flex flex-wrap gap-4 mt-4">
-                      <button
-                        type="button"
-                        className="w-12 h-12 bg-black border-2 border-white hover:border-gray-800 rounded-full shrink-0"
-                      ></button>
-                      <button
-                        type="button"
-                        className="w-12 h-12 bg-gray-300 border-2 border-white hover:border-gray-800 rounded-full shrink-0"
-                      ></button>
-                      <button
-                        type="button"
-                        className="w-12 h-12 bg-gray-100 border-2 border-white hover:border-gray-800 rounded-full shrink-0"
-                      ></button>
-                      <button
-                        type="button"
-                        className="w-12 h-12 bg-blue-400 border-2 border-white hover:border-gray-800 rounded-full shrink-0"
-                      ></button>
+                      {colors.map((color: any, index: number) => (
+                        <button
+                          key={index}
+                          onClick={() => {
+                            getProductImages({
+                              variables: {
+                                productId: params.productId,
+                                colorId: color.id,
+                              },
+                              onCompleted: (data) => {
+                                setSmallImages(data.getProductImages);
+                                setBigImage(data.getProductImages[0]);
+                              },
+                            });
+                          }}
+                          type="button"
+                          className={`w-12 h-12  bg-${color.color}-500 border-2 border-white hover:border-gray-800 rounded-lg shrink-0`}
+                        ></button>
+                      ))}
                     </div>
                   </div>
                   <h3 className="text-lg font-bold text-strongBeige mt-10">
@@ -301,7 +329,28 @@ const ProductDetails = ({ params }: { params: { productId: string } }) => {
           </div> */}
         </div>
       ) : (
-        <div> Loading ... </div>
+        <div
+          role="status"
+          className="flex items-center justify-center h-screen"
+        >
+          <svg
+            aria-hidden="true"
+            className="w-20 h-20 text-gray-200 animate-spin fill-strongBeige"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
       )}
     </>
   );
