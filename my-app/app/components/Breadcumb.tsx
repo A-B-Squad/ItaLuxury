c599c91440143be920398e8e2b6eb22ec9800fe0
Bilelbourgou <bilelbourgou@gmail.com>
2024-04-27 10:30:45 +0100

    all-products complelted

---
+++ my-app/app/components/Breadcumb.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { IoHome } from "react-icons/io5";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { IoMdArrowDropright } from "react-icons/io";
+
+const Breadcumb = () => {
+  const params = useSearchParams();
+  const position = params.getAll("collection");
+
+  return (
+    <div className="flex gap-2 items-center text-xs md:text-sm w-full  lg:w-max tracking-wider my-5 lg:ml-5">
+      <div className="hover:text-strongBeige transition-all  flex items-center gap-1">
+        <IoHome />
+        <Link href={"/Home"}>Accueil</Link>
+      </div>
+      {position[0] && (
+        <>
+          <IoMdArrowDropright size={20} />
+          <div className="hover:text-strongBeige transition-all flex items-center gap-2">
+            <Link href={`/Collections?collection=${position[0]}`}>
+              {position[0]}
+            </Link>
+          </div>
+        </>
+      )}
+      {position[1] && (
+        <>
+          <IoMdArrowDropright size={20} />
+          <div className="hover:text-strongBeige transition-all flex items-center gap-2">
+            <Link href={`/Collections?collection=${position[1]}`}>
+              {position[1]}
+            </Link>
+          </div>
+        </>
+      )}
+      {position[2] && (
+        <>
+          <IoMdArrowDropright size={20} />
+          <div className="text-strongBeige transition-all flex items-center gap-2">
+            <p>{position[2]} </p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Breadcumb;
--- my-app/app/components/allProducts/productsSection.tsx
+++ my-app/app/components/allProducts/productsSection.tsx
@@ -1,46 +1,29 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { convertStringToQueriesObject } from "./sideBar";
-import { useLazyQuery, useMutation } from "@apollo/client";
-import { BASKET_QUERY, SEARCH_PRODUCTS_QUERY } from "../../../graphql/queries";
+import { convertStringToQueriesObject } from "@/app/(mainApp)/Collections/_components/sideBar";
+import { useLazyQuery } from "@apollo/client";
+import { SEARCH_PRODUCTS_QUERY } from "../../../graphql/queries";
 import { FaHeart } from "react-icons/fa";
 import { SlBasket } from "react-icons/sl";
-import { ADD_TO_BASKET_MUTATION } from "@/graphql/mutations";
-import { useBasketStore, useProductsInBasketStore } from "@/app/store/zustand";
-import jwt, { JwtPayload } from "jsonwebtoken";
-import Cookies from "js-cookie";
+import Link from "next/link";
+import prepRoute from "../_prepRoute";
 
 const ProductsSection = () => {
   const searchParams = useSearchParams();
-
+  // const paramsObj = convertStringToQueriesObject(searchParams);
+  // const { color, category, price } = paramsObj;
   const colorParam = searchParams?.get("color");
   const categoryParam = searchParams?.get("category");
-  const priceParam = searchParams?.get("price");
-  const [productsData, setProductsData] = useState([]);
-  const [decodedToken, setDecodedToken] = useState<DecodedToken | null>(null);
-
+  const priceParamString = searchParams?.get("price");
+  const priceParam = priceParamString ? +priceParamString : undefined;
   const [searchProducts, { loading }] = useLazyQuery(SEARCH_PRODUCTS_QUERY);
-  const [addToBasket] = useMutation(ADD_TO_BASKET_MUTATION);
-
-  const toggleIsUpdated = useBasketStore((state) => state.toggleIsUpdated);
-  const { addProductToBasket, products } = useProductsInBasketStore(
-    (state) => ({
-      addProductToBasket: state.addProductToBasket,
-      products: state.products,
-    })
-  );
-
-  interface DecodedToken extends JwtPayload {
-    userId: string;
-  }
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const token = Cookies.get("Token");
-    if (token) {
-      const decoded = jwt.decode(token) as DecodedToken;
-      setDecodedToken(decoded);
-    }
     const fetchProducts = async () => {
+      console.log(categoryParam, colorParam, priceParam);
+
       try {
         const { data } = await searchProducts({
           variables: {
@@ -48,29 +31,39 @@ const ProductsSection = () => {
               categoryId: categoryParam || undefined,
               colorId: colorParam || undefined,
               minPrice: 1,
-              maxPrice: parseInt(priceParam || "500") || undefined,
+              maxPrice: priceParam || undefined,
             },
             page: 1,
             pageSize: 10,
           },
         });
-        setProductsData(data.searchProducts);
-      } catch (error) {}
+        console.log("fetched products:", data.searchProducts);
+        setProducts(data.searchProducts);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
     };
 
     fetchProducts();
   }, [searchProducts, colorParam, categoryParam, priceParam]);
 
   return (
-    <div className="flex flex-wrap gap-6">
-      {productsData.map((product: Product) => (
-        <div className="group my-10 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md">
-          <a className="relative flex h-60 overflow-hidden" href="#">
-            <img
-              className="absolute top-0 right-0 h-full w-full object-cover"
-              src={product.images[0]}
-              alt="product image"
-            />
+    <div className="w-full grid lg:grid-cols-5 px-10 gap-4 md:grid-cols-3 grid-cols-1">
+      {products.map((product: any) => (
+        <div className="group  my-10 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md">
+          <Link className="relative flex h-52 overflow-hidden" href="#">
+            <div className="group ">
+              <img
+                className="absolute group-hover:opacity-0 z-10 opacity-100 transition-all top-0 right-0 h-full w-full object-cover"
+                src={product.images[0]}
+                alt="product image"
+              />
+              <img
+                className="absolute group-hover:opacity-100 opacity-0 transition-all  top-0 right-0 h-full w-full object-cover"
+                src={product.images[1]}
+                alt="product image"
+              />
+            </div>
             <div className="absolute bottom-0 mb-4 flex w-full justify-center space-x-4">
               <div className="h-3 w-3 rounded-full border-2 border-white bg-white"></div>
               <div className="h-3 w-3 rounded-full border-2 border-white bg-transparent"></div>
@@ -81,67 +74,50 @@ const ProductsSection = () => {
                 <FaHeart />
               </button>
             </div>
-          </a>
+          </Link>
           <div className="mt-4 px-5 pb-5">
-            <a href="#">
-              <h5 className="text-xl tracking-tight text-slate-900">
-                {product.name}
-              </h5>
-            </a>
-            <div className="mt-2 mb-5 flex items-center justify-between">
-              <p>
-                <span className="text-3xl font-bold text-slate-900">
-                  {product.productDiscounts
-                    ? product.productDiscounts[0].newPrice.toFixed(3)
-                    : product.price.toFixed(3)}{" "}
-                  TND
-                </span>
-                {product.productDiscounts && (
-                  <span className="text-sm text-slate-900 line-through">
-                    {product.productDiscounts[0].price.toFixed(3)} TND
-                  </span>
-                )}
-              </p>
-            </div>
-            <button
-              className="flex items-center gap-3 justify-center bg-strongBeige px-2 py-1 text-lg text-white transition hover:bg-yellow-700"
-              onClick={() => {
-                if (decodedToken) {
-                  addToBasket({
-                    variables: {
-                      input: {
-                        userId: decodedToken?.userId,
-                        quantity: 1,
-                        productId: product.id,
-                      },
-                    },
-                    refetchQueries: [
-                      {
-                        query: BASKET_QUERY,
-                        variables: { userId: decodedToken?.userId },
-                      },
-                    ],
-                  });
-                } else {
-                  const isProductAlreadyInBasket = products.some(
-                    (p: any) => p.id === product.id
-                  );
-
-                  if (!isProductAlreadyInBasket) {
-                    addProductToBasket({
-                      ...product,
-                      price: product.productDiscounts
-                        ? product.productDiscounts[0].newPrice
-                        : product.price,
-                      quantity: 1,
-                    });
-                  } else {
-                    console.log("Product is already in the basket");
-                  }
-                }
-                toggleIsUpdated();
+            <Link
+             className="group"
+              href={{
+                pathname: `products/tunisie/${prepRoute(product?.name)}`,
+                query: {
+                  productId: product?.id,
+                  collection: [
+                    product?.categories[0]?.name,
+                    product?.categories[0]?.subcategories[0]?.name,
+                    product?.name,
+                  ],
+                },
               }}
             >
+              <h5 className="text-md group-hover:text-mediumBeige transition-colors tracking-tight text-slate-900">
+                {product.name}
+              </h5>
+            </Link>
+            <div
+            // className={`priceDetails ${product?.productDiscounts.length > 0 ? "group-hover:hidden" : "group-hover:translate-y-32 "}  translate-y-0`}
+          >
+            <p
+              className={`${
+                product?.productDiscounts.length > 0
+                  ? "line-through text-lg"
+                  : " text-strongBeige text-xl py-1"
+              }  font-semibold`}
+            >
+              {product?.price.toFixed(3)} TND
+            </p>
+            {product?.productDiscounts.length > 0 && (
+              <div className="flex items-center">
+                <span className="text-gray-400 text-xs font-thin">
+                  A partir de :
+                </span>
+                <span className="text-red-500 font-bold ml-1 text-xl">
+                  {product?.productDiscounts[0]?.newPrice.toFixed(3)} TND
+                </span>
+              </div>
+            )}
+          </div>
+            <button className="flex items-center gap-3 justify-center bg-strongBeige px-2 py-1 text-lg text-white transition hover:bg-yellow-700">
               <SlBasket />
               Ajouter au panier
             </button>
